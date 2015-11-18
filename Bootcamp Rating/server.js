4e4e0397e3e180f0c3d84b39c9eae94a95fea883
starcraft168 <ericeng168@gmail.com>
2015-11-18 10:11:50 -0800

    update

--- Bootcamp Rating/server.js
+++ Bootcamp Rating/server.js
@@ -13,6 +13,22 @@ app.use(express.static(__dirname + '/public'));
 var jsonParser = bodyParser.json(); //parses request.body (undefined String) into a JSON object
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
+// // PASSPORT JS
+// var passport = require('passport');
+// var GoogleStrategy = require('passport-google').Strategy;
+
+// passport.use(new GoogleStrategy({
+//     returnURL: 'http://www.example.com/auth/google/return',
+//     realm: 'http://www.example.com/'
+//   },
+//   function(identifier, profile, done) {
+//     User.findOrCreate({ openId: identifier }, function(err, user) {
+//       done(err, user);
+//     });
+//   }
+// ));
+
+
 // Retrieves the comments from the database
 app.get('/mainComments', function(req, res) {
 	db.mainComments.find(function(err, docs) {
@@ -43,6 +59,19 @@ app.delete('/mainComments/:id', function(req, res) {
 	db.mainComments.remove({_id: mongojs.ObjectId(id)}, function(err, doc) {
 		res.json(doc);
 	})
-})
+});
+
+// // Redirect the user to Google for authentication
+// app.get('/auth/google', passport.authenticate('google'));
+
+// // Google will redirect the user to this URL after authentication.  Finish
+// // the process by verifying the assertion.  If valid, the user will be
+// // logged in.  Otherwise, authentication has failed.
+// app.get('/auth/google/return',
+//   passport.authenticate('google', { successRedirect: '/',
+//                                     failureRedirect: '/login' }));
+
+
+
 
 app.listen(3000);
